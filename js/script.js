1cca400a0ebc60374333335e27a606cc2fe33c73
Ajoge Abdulmumin Adavize <84341034+ajogious@users.noreply.github.com>
2022-10-02 02:15:10 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+
+// JS Toggle menu----------------
+function showMenu(){
+   const navLinks = document.getElementById('navLinks');
+   navLinks.style.display = 'block';
+}
+function hideMenu(){
+   const navLinks = document.getElementById('navLinks');
+   navLinks.style.display = 'none';
+}
